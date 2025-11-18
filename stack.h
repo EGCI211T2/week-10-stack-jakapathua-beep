5c81b85f7b37f1b561c27e7e4be32ea602d25895
jakapathua-beep <jakapat.hua@student.mahidol.edu>
2025-11-18 03:50:32 +0000

    done

--- stack.h
+++ stack.h
@@ -1,10 +1,9 @@
-
 #ifndef stack_h
 #define stack_h
 #include "node.h"
 class Stack {
-	NodePtr top;
-	int size;
+    NodePtr top;
+    int size;
 public:
     void push(int);
     int pop();
@@ -16,30 +15,47 @@ public:
 void Stack::push(int x){
   NodePtr new_node=new NODE(x);
   if(new_node){
-            // Left missing for exercises…
+            new_node->set_next(top);
+            top= new_node;
+            size++;
    }
- 
-         // Left missing for exercises…
+   else cout<<"Not enough memory."<<endl;
     
 }
 
 int Stack::pop(){
         NodePtr t=top;
+    if(t!=NULL){
         int value;
         value=t->get_value();
-    // Left missing part for exercises
+
+        //move top to the next one
+        top=top->get_next();
+        
+        --size;
+        
         delete t;
         return value;
-	//be careful of the empty stack!!!
+    
     }
+   cout<<"Empty stack"<<endl;
+    return 0;
+}
 
 Stack::Stack(){
     //initialize stack
+    top = NULL;
+    size = 0;
     
 }
 Stack::~Stack(){
     //delete all remaning stack (i.e. pop all) 
-    
+    cout<<"Clear the stack"<<endl;
+    int n = size;
+    while(n>0){
+    pop();
+    n--;
+    }
 }
 
 
